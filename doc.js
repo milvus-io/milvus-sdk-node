b5074db23990def5c6489c070d652b9727b5ecde
nameczz <zizhao.chen@zilliz.com>
2021-08-12 17:42:42 +0800

    change api folder name

--- doc.js
+++ doc.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const formatDoc = async () => {
   fs.unlinkSync("./docs/index.html");
   fs.unlinkSync("./docs/modules.html");
-  fs.mkdirSync("./docs/api");
+  fs.mkdirSync("./docs/Api Reference");
   fs.renameSync(
     "./docs/modules/documentation.html",
-    "./docs/api/documentation.html"
+    "./docs/Api Reference/documentation.html"
   );
   fs.rmdirSync("./docs/modules", { recursive: true });
 };
