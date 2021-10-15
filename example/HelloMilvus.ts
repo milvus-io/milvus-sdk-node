3a721524970e99861f6c1cf5af09cd0295a96935
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2021-10-15 10:27:51 +0300

    Add Index, Load and Search to HelloMilvus
    Signed-off-by: GuyKhmel <3136012+GuyKh@users.noreply.github.com>

--- example/HelloMilvus.ts
+++ example/HelloMilvus.ts
@@ -73,14 +73,6 @@ const hello_milvus = async () => {
     });
     console.log("--- Has collection (" + collectionName + ") ---", hasCollectionRes);
 
-
-    await milvusClient.partitionManager.createPartition({
-      collection_name: collectionName,
-      partition_name: "test",
-    });
-
-    console.log("--- Create Partition in Collection ---", collectionName, "test");
-
   
     const fields = [
       {
@@ -93,24 +85,52 @@ const hello_milvus = async () => {
         name: "random_value",
       },
     ];
-    const vectorsData = generateInsertData(fields, 1000);
+    const vectorsData = generateInsertData(fields, 100);
   
     const params: InsertReq = {
       collection_name: collectionName,
-      fields_data: vectorsData,
-      partition_name: "test",
+      fields_data: vectorsData
     };
   
     await milvusClient.dataManager.insert(params);
     console.log("--- Insert Data to Collection ---");
 
-
-    const queryRes = await milvusClient.dataManager.query({
+  
+    await milvusClient.indexManager.createIndex({
       collection_name: collectionName,
-      expr: "count < 10",
-      output_fields: ["count", "random_value", "float_vector"]
+      field_name: "float_vector",
+      extra_params: {
+        index_type: "IVF_FLAT",
+        metric_type: "L2",
+        params: JSON.stringify({ nlist: 10 }),
+      },
     });
-    console.log("--- Query Collection ---", queryRes);
+    console.log("--- Create Index in Collection ---");
+  
+  
+        // need load collection before search
+    let loadCollectionRes = await collectionManager.loadCollectionSync({
+      collection_name: collectionName,
+    });
+    console.log("--- Load collection (" + collectionName + ") ---", loadCollectionRes);
+
+  
+    const result = await milvusClient.dataManager.search({
+      collection_name: collectionName,
+      vectors: [vectorsData[0]["float_vector"]],
+      search_params: {
+        anns_field: "float_vector",
+        topk: "4",
+        metric_type: "L2",
+        params: JSON.stringify({ nprobe: 1024 }),
+        round_decimal: 4,
+      },
+      output_fields: ["count"],
+      vector_type: DataType.FloatVector,
+    });
+  
+    console.log("--- Search collection (" + collectionName + ") ---", result);
+
 
     const releaseRes = await collectionManager.releaseCollection({
       collection_name: collectionName,
