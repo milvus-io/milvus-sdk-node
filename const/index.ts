3c9c52b0b930c1158f9ea977890b900e5b10e509
shanghaikid <jiangruiyi@gmail.com>
2023-02-23 17:33:09 +0800

    fix test

--- const/index.ts
+++ const/index.ts
@@ -1,6 +1,6 @@
 // export const IP = "172.16.50.14:19530";
-export const IP = "172.16.50.11:19530";
-// export const IP = "127.0.0.1:19530";
+// export const IP = "172.16.50.11:19530";
+export const IP = "127.0.0.1:19530";
 
 export const DIMENSION = 4;
 
