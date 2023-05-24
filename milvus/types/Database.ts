f9fae1e5db9ad6985755360344254860c31527b8
shanghaikid <jiangruiyi@gmail.com>
2023-05-24 20:21:10 +0800

    remove unused type
    Signed-off-by: shanghaikid <jiangruiyi@gmail.com>

--- milvus/types/Database.ts
+++ milvus/types/Database.ts
@@ -1,9 +1,5 @@
 import { GrpcTimeOut, ResStatus } from './Common';
 
-export interface DatabaseNameReq {
-  db_name?: string;
-}
-
 export interface CreateDatabaseRequest extends GrpcTimeOut {
   db_name: string;
 }
