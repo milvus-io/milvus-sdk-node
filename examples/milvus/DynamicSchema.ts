8daf16860b4cbe6c306f66fd135b8a76615080b7
shanghaikid <jiangruiyi@gmail.com>
2023-06-09 12:05:36 +0800

    update examples
    Signed-off-by: shanghaikid <jiangruiyi@gmail.com>

--- examples/milvus/DynamicSchema.ts
+++ examples/milvus/DynamicSchema.ts
@@ -1,6 +1,10 @@
-import { MilvusClient, InsertReq, DataType } from '@zilliz/milvus2-sdk-node';
+import {
+  MilvusClient,
+  InsertReq,
+  ConsistencyLevelEnum,
+} from '@zilliz/milvus2-sdk-node';
 
-const COLLECTION_NAME = 'partition_key_example';
+const COLLECTION_NAME = 'dynamic_schema_example';
 
 // milvus v2.2.9 only
 
@@ -13,32 +17,10 @@ const COLLECTION_NAME = 'partition_key_example';
   });
 
   console.log('Node client is initialized.');
-  // create collection
+  // create collection / create index / load in one function
   const create = await milvusClient.createCollection({
     collection_name: COLLECTION_NAME,
-    fields: [
-      {
-        name: 'age',
-        description: 'ID field',
-        data_type: DataType.Int64,
-        is_primary_key: true,
-        autoID: true,
-      },
-      {
-        name: 'vector',
-        description: 'Vector field',
-        data_type: DataType.FloatVector,
-        dim: 8,
-      },
-      { name: 'height', description: 'int64 field', data_type: DataType.Int64 },
-      {
-        name: 'name',
-        description: 'VarChar field',
-        data_type: DataType.VarChar,
-        max_length: 128,
-        is_partition_key: true, // partition key enbabled field
-      },
-    ],
+    dimension: 8,
   });
   console.log('Create collection is finished.', create);
 
@@ -50,8 +32,10 @@ const COLLECTION_NAME = 'partition_key_example';
         0.5484226189097237, 0.9839246709011924, 0.5178387104937776,
         0.8716926129208069, 0.5616972243831446,
       ],
-      height: 20405,
-      name: 'apple',
+      id: 20405,
+      name: 'zlnmh',
+      height: 185,
+      age: 22,
     },
     {
       vector: [
@@ -59,8 +43,10 @@ const COLLECTION_NAME = 'partition_key_example';
         0.09946359318481224, 0.6790698063908669, 0.5013786801063624,
         0.795311915725105, 0.9183033261617566,
       ],
-      height: 93773,
-      name: 'apple',
+      id: 93773,
+      name: '5lr9y',
+      height: 170,
+      age: 45,
     },
     {
       vector: [
@@ -68,8 +54,8 @@ const COLLECTION_NAME = 'partition_key_example';
         0.5619757601304878, 0.6076543120476996, 0.8373907516027586,
         0.8556140171597648, 0.4043893119391049,
       ],
-      height: 85122,
-      name: 'apple',
+      id: 85122,
+      name: 'nes0j',
     },
     {
       vector: [
@@ -77,8 +63,10 @@ const COLLECTION_NAME = 'partition_key_example';
         0.7996354835509332, 0.8207766774911736, 0.38133662902290566,
         0.7576720055508186, 0.4393152967662368,
       ],
-      height: 92037,
-      name: 'banana',
+      id: 92037,
+      name: 'ct2li',
+      height: 166,
+      age: 32,
     },
     {
       vector: [
@@ -86,8 +74,9 @@ const COLLECTION_NAME = 'partition_key_example';
         0.31693696726284193, 0.3731715403499176, 0.3300751870649885,
         0.22353556137796238, 0.38062799545615444,
       ],
-      height: 31400,
+      id: 31400,
       name: '6ghrg',
+      age: 45,
     },
     {
       vector: [
@@ -95,8 +84,9 @@ const COLLECTION_NAME = 'partition_key_example';
         0.3795768837758642, 0.4532443258064389, 0.596455163143,
         0.9529469158782906, 0.7692465408044873,
       ],
-      height: 1778,
+      id: 1778,
       name: 'sb7mt',
+      money: 1234,
     },
   ];
   const params: InsertReq = {
@@ -107,45 +97,19 @@ const COLLECTION_NAME = 'partition_key_example';
   await milvusClient.insert(params);
   console.log('Data is inserted.');
 
-  // create index
-  const createIndex = await milvusClient.createIndex({
-    collection_name: COLLECTION_NAME,
-    field_name: 'vector',
-    metric_type: 'L2',
-  });
-
-  console.log('Index is created', createIndex);
-
-  // need load collection before search
-  const load = await milvusClient.loadCollectionSync({
-    collection_name: COLLECTION_NAME,
-  });
-  console.log('Collection is loaded.', load);
-
   // do the search
   for (let i = 0; i < 1; i++) {
     console.time('Search time');
     const search = await milvusClient.search({
       collection_name: COLLECTION_NAME,
       vector: vectorsData[i]['vector'],
-      filter: 'name in ["apple"]',
-      output_fields: ['name'],
+      output_fields: ['age', 'money', 'height'],
       limit: 5,
+      consistency_level: ConsistencyLevelEnum.Strong,
     });
     console.timeEnd('Search time');
     console.log('Search result', search);
   }
-
-  // do the query
-  console.time('Query time');
-  const query = await milvusClient.query({
-    collection_name: COLLECTION_NAME,
-    filter: 'name in ["apple"]',
-    output_fields: ['name', 'vector'],
-    limit: 100,
-  });
-  console.timeEnd('Query time');
-  console.log('query result', query);
 
   // drop collection
   await milvusClient.dropCollection({
