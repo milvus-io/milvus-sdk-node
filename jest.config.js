b75fa28a3bd9b20048d206a52ebe3bf025481971
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2023-10-25 20:11:16 +0800

    update test config
    Signed-off-by: ruiyi.jiang <ruiyi.jiang@zilliz.com>

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
   testTimeout: 60000,
   // because user will cause other test fail, but we still have user spec
   coveragePathIgnorePatterns: ['/milvus/User.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/test/cloud.spec.test',
+    '<rootDir>/test/serverless.spec.test',
+  ], // add this line
   testEnvironmentOptions: {
     NODE_ENV: 'production',
   },
