14d0a7d05134c0cb282d0054bea7fe4ffcbf1bcd
Shuyoou <shuyoou@outlook.com>
2024-02-02 10:46:40 +0800

    feat(example): upgrade nextjs (#277)

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,5 +1,11 @@
 import { MilvusClient } from '@zilliz/milvus2-sdk-node';
 
+/**
+ * This option is equivalent to getServerSideProps() in the pages directory.
+ * https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+ */
+export const dynamic = 'force-dynamic';
+
 async function getData() {
   const milvusClient = new MilvusClient({
     address: '10.102.6.196:19530',
