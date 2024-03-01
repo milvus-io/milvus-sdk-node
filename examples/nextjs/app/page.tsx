b8a8e508a690d76cddea34ee21ab2a0ba9739ef2
Shuyoou <shuyou.zhan@zilliz.com>
2024-03-01 18:57:06 +0800

    fix: vercel deploy error (#281)

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,13 +1,19 @@
-/**
- * This option is equivalent to getServerSideProps() in the pages directory.
- * https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
- */
-export const dynamic = 'force-dynamic';
-
-const HOST = 'http://localhost:3000';
+import { MilvusClient } from '@zilliz/milvus2-sdk-node';
 
 async function getData() {
-  return fetch(HOST + '/api/milvus').then(res => res.json());
+  const milvusClient = new MilvusClient({
+    address: '10.102.6.196:19530',
+  });
+
+  let res: any = await milvusClient.getMetric({
+    request: { metric_type: 'system_info' },
+  });
+
+  const result = res.response.nodes_info.map((v: any) => {
+    return v.infos;
+  });
+
+  return result;
 }
 
 export default async function Home() {
