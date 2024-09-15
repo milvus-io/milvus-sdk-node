9726be3c949bde309efa7159bd45ba780442beb7
shanghaikid <jiangruiyi@gmail.com>
2024-09-15 14:53:04 +0800

    remove console
    Signed-off-by: shanghaikid <jiangruiyi@gmail.com>

--- test/grpc/PartialLoad.spec.ts
+++ test/grpc/PartialLoad.spec.ts
@@ -1,7 +1,7 @@
 import { MilvusClient, ErrorCode, DataType } from '../../milvus';
 import { IP, GENERATE_NAME, generateInsertData } from '../tools';
 
-const milvusClient = new MilvusClient({ address: IP, logLevel: 'debug' });
+const milvusClient = new MilvusClient({ address: IP, logLevel: 'info' });
 const COLLECTION_NAME = GENERATE_NAME();
 const schema = [
   {
