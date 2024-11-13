4f2377552411f47acc7908621c37909d9a4ac64e
rashgaroth <50513263+rashgaroth@users.noreply.github.com>
2024-11-13 09:46:37 +0800

    Remove lock files
    This reverts commit 8315115f9a6981a615e46fcbda840879726e681e.

--- milvus/grpc/Data.ts
+++ milvus/grpc/Data.ts
@@ -67,7 +67,6 @@ import {
   f32ArrayToBinaryBytes,
   getValidDataArray,
   NO_LIMIT,
-  DescribeCollectionReq,
 } from '../';
 import { Collection } from './Collection';
 
@@ -466,19 +465,11 @@ export class Data extends Collection {
   async search(
     data: SearchReq | SearchSimpleReq | HybridSearchReq
   ): Promise<SearchResults> {
-    // default collection request
-    const describeCollectionRequest = {
+    // get collection info
+    const collectionInfo = await this.describeCollection({
       collection_name: data.collection_name,
       cache: true,
-    } as DescribeCollectionReq;
-
-    // get collection info
-    if (data.db_name) {
-      // if the request has `db_name` pass it to the request.
-      describeCollectionRequest.db_name = data.db_name;
-    }
-
-    const collectionInfo = await this.describeCollection(describeCollectionRequest);
+    });
 
     // build search params
     const { request, nq, round_decimal, isHybridSearch } = buildSearchRequest(
