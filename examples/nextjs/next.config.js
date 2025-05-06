98f8629bd56c9807a758a89cdbe37068efb51806
ryjiang <jiangruiyi@gmail.com>
2025-05-06 15:48:05 +0800

    chore: update vercel config
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@zilliz/milvus2-sdk-node'],
+  serverExternalPackages: ["@zilliz/milvus2-sdk-node"],
+  outputFileTracingIncludes: {
+    // When deploying to Vercel, the following configuration is required
+    "/api/**/*": ["node_modules/@zilliz/milvus2-sdk-node/dist/proto/**/*"],
+  },
 };
 
 module.exports = nextConfig;
