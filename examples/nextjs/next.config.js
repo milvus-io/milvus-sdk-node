fa385773aca0dd83a6d88c5745fd4a8b1e4d8ec1
ryjiang <jiangruiyi@gmail.com>
2025-05-06 14:36:47 +0800

    chore: upgrade nextjs example
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@zilliz/milvus2-sdk-node"],
-  },
+  serverExternalPackages: ['@zilliz/milvus2-sdk-node'],
 };
 
 module.exports = nextConfig;
